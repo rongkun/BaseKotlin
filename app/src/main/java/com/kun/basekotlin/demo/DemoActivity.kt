219003c7deec558f7da06bed55f7f6f81ce6da34
缘曦之梦 <2514375586@qq.com>
2017-11-01 11:54:21 +0800

    使用anko进行布局

--- app/src/main/java/com/kun/basekotlin/demo/DemoActivity.kt
+++ app/src/main/java/com/kun/basekotlin/demo/DemoActivity.kt
@@ -7,9 +7,14 @@ import com.kun.baselib.base.BaseActivity
 import com.kun.baselib.base.BaseApplication
 import com.kun.baselib.utils.ToastUtil
 import kotlinx.android.synthetic.main.activity_demo.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 import javax.inject.Inject
 
 class DemoActivity : BaseActivity(),DemoContract.View {
+    companion object static{
+        val ID_TEXT : Int = 1
+    }
     override fun getWeatherSuccess(weatherResponse: WeatherResponse) {
         ToastUtil.toastShort("温度："+weatherResponse.wendu)
     }
@@ -33,9 +38,26 @@ class DemoActivity : BaseActivity(),DemoContract.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_demo)
-        tvTest.text = "Hello Kotlin"
-        btTest.setOnClickListener { mPresenter.getWeather("广州") }
+//        setContentView(R.layout.activity_demo)
+//        tvTest.text = "Hello Kotlin"
+//        btTest.setOnClickListener { }
         mPresenter.send()
+        relativeLayout {
+            textView ("hello anko"){
+                id = ID_TEXT
+                textSize = 16f
+            }.lparams(wrapContent){
+                centerInParent()
+            }
+            button("获取温度"){
+                textSize = 14f
+            }.lparams(wrapContent){
+                below(ID_TEXT)
+                centerHorizontally()
+                topMargin = 10
+            }.onClick {
+                mPresenter.getWeather("广州")
+            }
+        }
     }
 }
